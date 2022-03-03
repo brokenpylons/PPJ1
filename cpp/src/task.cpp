6a7f1c4155a760b2d08636a46909f875ec7dcd7d
Žiga Leber <zigaleber7@gmail.com>
2022-03-03 14:11:56 +0100

    Update task.cpp

--- cpp/src/task.cpp
+++ cpp/src/task.cpp
@@ -7,5 +7,5 @@ int main(int argc, char** argv)
     std::ifstream ifs(argv[1]);
     std::string input;
     ifs >> input;
-    std::cout << "ANSWER" << input << std::endl;
+    std::cout << input << std::endl;
 }
